4778113ca6c63c9c35c11425aef6ef262a2246b0
Evgeny Segeda <evgeny.segeda@sap.com>
2019-09-13 17:20:30 +0200

    Add sample app

---
+++ webapp/Component.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/core/ComponentSupport"
+ ], function (UIComponent) {
+    "use strict";
+    return UIComponent.extend("openui5.project.Component", {
+        metadata : {
+            manifest: "json"
+        }
+    });
+ });
